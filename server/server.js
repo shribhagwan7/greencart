ce543c08288cca33012de64302942706b7321e7c
ShriBhagwan7 <official.shribhagwan7@gmail.com>
2025-06-29 13:08:08 +0530

    Fixed login issue with cookies and CORS

--- server/server.js
+++ server/server.js
@@ -17,11 +17,16 @@ const port = process.env.PORT || 4000;
 await connectDB()
 await connectCloudinary()
 
-const allowedOrigins = ['http://localhost:5173'];
+const allowedOrigins = ['http://localhost:5173', 'https://greencart-henna.vercel.app'];
+
+app.use(cors({
+    origin: allowedOrigins,
+    credentials: true
+}));
+
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: allowedOrigins, credentials: true }));
 
 app.get('/', (req, res) => res.send("API IS WORKING"));
 app.use('/api/user', userRouter)
